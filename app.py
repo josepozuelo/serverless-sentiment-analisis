02d1055813774f53e32e674878cbf0b46d12bf0b
josepozuelo <56085442+josepozuelo@users.noreply.github.com>
2022-10-19 22:45:03 -0600

    Update app.py
    Add logic for cardiffnlp
    /
    twitter-roberta-base-sentiment

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from transformers import pipeline
-import torch
+from transformers import AutoModelForSequenceClassification
+from transformers import TFAutoModelForSequenceClassification
+from transformers import AutoTokenizer
+import numpy as np
+from scipy.special import softmax
+import csv
+import urllib.request
+
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
@@ -7,20 +13,55 @@ def init():
     global model
     
     device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+    # Tasks:
+    # emoji, emotion, hate, irony, offensive, sentiment
+    # stance/abortion, stance/atheism, stance/climate, stance/feminist, stance/hillary
+
+    task='sentiment'
+    MODEL = f"cardiffnlp/twitter-roberta-base-{task}"
+
+    tokenizer = AutoTokenizer.from_pretrained(MODEL)
+
+    # download label mapping
+    labels=[]
+    mapping_link = f"https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/{task}/mapping.txt"
+    with urllib.request.urlopen(mapping_link) as f:
+        html = f.read().decode('utf-8').split("\n")
+        csvreader = csv.reader(html, delimiter='\t')
+    labels = [row[1] for row in csvreader if len(row) > 1]
+    # PT
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+    model.save_pretrained(MODEL)
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
 def inference(model_inputs:dict) -> dict:
     global model
-
-    # Parse out your arguments
-    prompt = model_inputs.get('prompt', None)
+    text = model_inputs.get('prompt', None)
     if prompt == None:
-        return {'message': "No prompt provided"}
-    
-    # Run the model
-    result = model(prompt)
+            return {'message': "No prompt provided"}
+    text = preprocess(text)
+    encoded_input = tokenizer(text, return_tensors='pt')
+    output = model(**encoded_input)
+    scores = output[0][0].detach().numpy()
+    scores = softmax(scores)
 
-    # Return the results as a dictionary
-    return result
+    # # TF
+    # model = TFAutoModelForSequenceClassification.from_pretrained(MODEL)
+    # model.save_pretrained(MODEL)
+
+    # text = "Good night 😊"
+    # encoded_input = tokenizer(text, return_tensors='tf')
+    # output = model(encoded_input)
+    # scores = output[0][0].numpy()
+    # scores = softmax(scores)
+
+    ranking = np.argsort(scores)
+    ranking = ranking[::-1]
+    for i in range(scores.shape[0]):
+        l = labels[ranking[i]]
+        s = scores[ranking[i]]
+        print(f"{i+1}) {l} {np.round(float(s), 4)}")
+
+    return ranking
+
